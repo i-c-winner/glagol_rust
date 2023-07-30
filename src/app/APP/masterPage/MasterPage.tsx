b193cab8535c7661c3c7d7d2ed694c4ed2f9eb9d
Dmitriy Belousov <dmitriy@pop-os.localdomain>
2023-07-30 11:53:21 +0500

    feat (MasterPage): Подключит асинхрон для strophe регистрации

--- src/app/APP/masterPage/MasterPage.tsx
+++ src/app/APP/masterPage/MasterPage.tsx
@@ -1,11 +1,86 @@
-import {Main} from "../../../pages";
-import {peerConnection} from "../../../entities";
-import { useEffect } from "react";
+import { Main } from "../../../pages";
+import { peerConnection } from "../../../entities";
+import { useEffect, useState } from "react";
+import * as strophe from "strophe.js"
+import { config, setRegister } from "../../../shared";
+import { useAsync } from "react-async";
+import { getRandomText } from "../../../shared";
 
-function MasterPage() {
-  return (
-   <Main />
-  )
+const { Strophe } = strophe
+const pc: RTCPeerConnection= new RTCPeerConnection({
+  iceServers:[
+    {
+      urls: config.peerServer
+    }
+  ]
+})
+pc.onicecandidate=((event)=>{
+  if (event.candidate) {
+
+  }
+})
+setRegister(strophe)
+const userId = getRandomText(5)
+const password = getRandomText(8)
+// const connect=new Promise((resolve: any, reject: any) =>{
+//   resolve (new Strophe.Connection(config.xmppUrls))
+// })
+const connect = async ()=>{
+  const connection= await  new Strophe.Connection(config.xmppUrls)
+  console.log(connection)
+  return connection
+}
+
+
+
+const  MasterPage=()=> {
+  console.log('masterPage')
+  const [ connected, setConnected] = useState(false)
+const {data, error, isPending}= useAsync({promiseFn: connect})
+  if (data) {
+    const connection = data
+    function callback(status: number) {
+      //@ts-ignore
+      if (status === Strophe.Status.REGISTER) {
+        // fill out the fields
+        connection.register.fields.username = userId;
+        connection.register.fields.password = password;
+        // calling submit will continue the registration process
+        connection.register.submit();
+        //@ts-ignore
+      } else if (status === Strophe.Status.REGISTERED) {
+        console.log("registered!");
+        // calling login will authenticate the registered JID.
+        connection.authenticate();
+        //@ts-ignore
+      } else if (status === Strophe.Status.CONFLICT) {
+        console.log("Contact already existed!");
+        //@ts-ignore
+      } else if (status === Strophe.Status.NOTACCEPTABLE) {
+        console.log("Registration form not properly filled out.")
+        //@ts-ignore
+      } else if (status === Strophe.Status.REGIFAIL) {
+        console.log("The Server does not support In-Band Registration")
+      } else if (status === Strophe.Status.CONNECTED) {
+        // @ts-ignore
+        window.glagol.connection = connection
+        // @ts-ignore
+        window.glagol.user = {
+          userId,
+          password
+        }
+        // @ts-ignore
+        setConnected(true)
+        // do something after successful authentication
+      } else {
+        // Do other stuff
+      }
+    }
+    connection.register.connect('prosolen.net', callback)
+  }
+  {
+    return connected ? <Main/> : null
+  }
 }
 
 export default MasterPage
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import route from "./route/route";
@@ -8,9 +7,7 @@ import { ThemeProvider } from "@mui/material";
 
 // @ts-ignore
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <RouterProvider router={route}/>
     </ThemeProvider>
-  </React.StrictMode>
 )
